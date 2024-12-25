be78fc7304fc2dd48c9410e4ca8cf3acb3a60af9
Priyangsu Banik <priyangsubanik2003@gmail.com>
2024-12-25 23:13:40 +0530

    postgres configured

--- src/components/Providers/RootProviders.tsx
+++ src/components/Providers/RootProviders.tsx
@@ -10,7 +10,16 @@ interface RootProvidersProps {
 }
 
 const RootProviders: FC<RootProvidersProps> = ({ children }) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
   return (
     <>
       <QueryClientProvider client={queryClient}>
